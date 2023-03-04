bfecc3261e78f7d0dc54acc278041463f6931b14
Chidike Henry <dk1online25@gmail.com>
2023-03-04 22:45:34 +0100

    added new feat

--- src/app/components/navbar/navitems.tsx
+++ src/app/components/navbar/navitems.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import styled from "styled-components";
-
+import tw from "twin.macro";
 
 const ListContainer = styled.ul`
     ${tw`
     flex
+    list-none
     `}
 
+`;
+
+const NavItem = styled.li`
+
+    ${tw`
+
+    `}
 `;
 
 export function NavItems() {
