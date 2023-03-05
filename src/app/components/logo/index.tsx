13d026d09bc302274c1141d00defc67cc5084ae5
Chidike Henry <dk1online25@gmail.com>
2023-03-05 15:39:41 +0100

    added new feat

--- src/app/components/logo/index.tsx
+++ src/app/components/logo/index.tsx
@@ -2,47 +2,50 @@ import React from "react";
 import styled from "styled-components";
 import CarLogoImg from "../../../assets/images/car-logo.png";
 import tw from "twin.macro";
+import CarLogoDarkImg from "../../../assets/images/car-logo-dark.png";
+
+interface ILogoProps {
+  color?: "white" | "dark";
+  bgColor?: "white" | "dark";
+}
 
 const LogoContainer = styled.div`
-    ${tw`
+  ${tw`
     flex
     items-center
-    `};
+  `};
 `;
 
 const LogoText = styled.div`
-    ${tw`
-        text-xl
-        md:text-2xl
-        font-bold
-        text-black
-        m-1
+  ${tw`
+    text-xl
+    md:text-2xl
+    font-bold
+    text-black
+    m-1
     `};
-`;
+  ${({ color }: any) => (color === "white" ? tw`text-white` : tw`text-black`)}
+` as any;
 
 const Image = styled.div`
+  width: auto;
+  ${tw`h-6 md:h-9`};
 
+  img {
     width: auto;
-    ${tw`
-        h-6
-        md:h-9
-    `};
-
-    img {
-        width: auto;
-        height: 100%;
-    }
+    height: 100%;
+  }
 `;
 
+export function Logo(props: ILogoProps) {
+  const { color, bgColor } = props;
 
-
-
-export function Logo() {
-    return <LogoContainer>
-        <Image>
-            <img src={CarLogoImg} />
-        </Image>
-        <LogoText>GlobalCars.</LogoText>
-
+  return (
+    <LogoContainer>
+      <Image>
+        <img src={bgColor === "dark" ? CarLogoDarkImg : CarLogoImg} />
+      </Image>
+      <LogoText color={color || "dark"}>GlobalCars.</LogoText>
     </LogoContainer>
+  );
 }
--- src/app/components/navbar/navitems.tsx
+++ src/app/components/navbar/navitems.tsx
@@ -1,21 +1,21 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import tw from "twin.macro";
-import { Slide as Menu } from "react-burger-menu";
-import { useMediaQuery }from "react-responsive"
+import { slide as Menu } from "react-burger-menu";
+import { useMediaQuery } from "react-responsive";
+import { SCREENS } from "../responsive";
+import menuStyles from "./menuStyle";
 
 const ListContainer = styled.ul`
-    ${tw`
+  ${tw`
     flex
     list-none
-    `}
-
+  `};
 `;
 
-const NavItem = styled.li`
-
-    ${tw`
-    text-xs
+const NavItem = styled.li<{ menu?: any }>`
+  ${tw`
+    text-sm
     md:text-base
     text-black
     font-medium
@@ -26,35 +26,57 @@ const NavItem = styled.li`
     duration-300
     ease-in-out
     hover:text-gray-700
-    `}
+  `};
+
+  ${({ menu }) =>
+    menu &&
+    css`
+      ${tw`
+      text-white
+      text-xl
+      mb-3
+      focus:text-white
+    `};
+    `};
 `;
 
 export function NavItems() {
+  const isMobile = useMediaQuery({ maxWidth: SCREENS.sm });
 
-    const isMobile = useMediaQuery({ maxWidth: SCREENS.sm });
-    
-    if (isMobile) {
-        return (
-            <Menu right>
-                <a href="#">Home</a>
-                <a href="#">Cars</a>
-                <a href="#">Services</a>
-                <a href="#">Contact Us</a>
-            </Menu>
-        )
-    }
-    return (<ListContainer>
-        <NavItem>
+  if (isMobile)
+    return (
+      <Menu right styles={menuStyles}>
+        <ListContainer>
+          <NavItem menu>
             <a href="#">Home</a>
-        </NavItem>
-        <NavItem>
+          </NavItem>
+          <NavItem menu>
             <a href="#">Cars</a>
-        </NavItem>
-        <NavItem>
+          </NavItem>
+          <NavItem menu>
             <a href="#">Services</a>
-        </NavItem>
-        <NavItem>
+          </NavItem>
+          <NavItem menu>
             <a href="#">Contact Us</a>
-        </NavItem>
-    </ListContainer>)
+          </NavItem>
+        </ListContainer>
+      </Menu>
+    );
+
+  return (
+    <ListContainer>
+      <NavItem>
+        <a href="#">Home</a>
+      </NavItem>
+      <NavItem>
+        <a href="#">Cars</a>
+      </NavItem>
+      <NavItem>
+        <a href="#">Services</a>
+      </NavItem>
+      <NavItem>
+        <a href="#">Contact Us</a>
+      </NavItem>
+    </ListContainer>
+  );
 }
